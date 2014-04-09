4776164aa44c89d46535413b65ba50c1093646db
Stefano <l.stickell@yahoo.it>
2014-04-09 15:08:13 +0200

    Travis test

---
+++ tests/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+./pyLoadCore.py -v
+./pyLoadCore.py --daemon -c -d
+until [ "`./pyLoadCli.py -u pyload --pw=pyload status`" == "No downloads running." ]
+do
+	sleep 1
+done
+./pyLoadCli.py -u pyload --pw=pyload add Test http://proof.ovh.net/files/10Mio.dat
+echo "Added download test"
+sleep 5
+until [ "`./pyLoadCli.py -u pyload --pw=pyload status`" == "No downloads running." ]
+do
+	./pyLoadCli.py -u pyload --pw=pyload status
+	sleep 5
+done
+echo "Download test completed"
+if [[ "`./pyLoadCli.py -u pyload --pw=pyload queue`" == *failed* ]]
+then
+	echo "Download failed"
+	./pyLoadCore.py --quit
+	exit 1
+fi
+./pyLoadCore.py --quit
